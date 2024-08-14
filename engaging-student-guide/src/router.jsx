c25645680c1236b9b2dbbf39a612d0ffa1a657ee
debruin09 <st.kluedbruin09@gmail.com>
2024-08-14 16:33:29 +0200

    added challenges page

--- engaging-student-guide/src/router.jsx
+++ engaging-student-guide/src/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-// import Challenges from "./routes/Challenges.jsx";
+import Challenges from "./routes/Challenges.jsx";
 import Favourites from "./routes/Favourites.jsx";
 import Home from "./routes/Home.jsx";
 import Quiz from "./routes/Quiz.jsx";
@@ -15,10 +15,10 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
-    //   {
-    //     path: "/challenges",
-    //     element: <Challenges />,
-    //   },
+      {
+        path: "/weekly",
+        element: <Challenges />,
+      },
       {
         path: "/favourites",
         element: <Favourites />,
